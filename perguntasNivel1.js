943c36a869d5a30043eee816b076c0eafdcb3bed
vinnidias <vinicius.3991@gmail.com>
2020-05-19 13:34:24 -0300

    listas de perguntas nivel 1

--- perguntasNivel1.js
+++ perguntasNivel1.js
@@ -1,45 +1,370 @@
-const perguntasNivel1 = [
+
+const perguntasListal = [
     0= {
-        pergunta: '',
+        pergunta: 'Em que estado brasileiro nasceu a apresentadora Xuxa?',
         alternativas: {
-            a: '',
-            b: '',
-            c: '',
-            d: '',  
+            a: 'RIO DE JANEIRO',
+            b: 'RIO GRANDE DO SUL',
+            c: 'SANTA CATARINA',
+            d: 'GOIÁS',  
         },
-        resposta: ''
+        resposta: alternativas.b
     
     },
 
+    1= {
+        pergunta: 'Qual é o nome dado ao estado da água em forma de gelo?',
+        alternativas: {
+            a: 'LÍQUIDO',
+            b: 'SÓLIDO',
+            c: 'GASOSO',
+            d: 'VAPOROSO',  
+        },
+        resposta: alternativas.b
+    
+    },
+    
+    2= {
+        pergunta: 'Qual era o apelido da cantora Elis Regina?',
+        alternativas: {
+            a: 'GAUCHINHA',
+            b: 'PAULISTINHA',
+            c: 'PIMENTINHA',
+            d: 'ANDORINHA',  
+        },
+        resposta: alternativas.c
+    
+    },
+
+    3= {
+        pergunta: 'Quem é a namorada do Mickey?',
+        alternativas: {
+            a: 'MARGARIDA',
+            b: 'MINNIE',
+            c: 'A PEQUENA SEREIA',
+            d: 'OLÍVIA PALITO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    4= {
+        pergunta: 'Qual é o personagem do folclore brasileiro que tem umaperna só?',
+        alternativas: {
+            a: 'CUCA',
+            b: 'NEGRINHO DO PASTOREIO',
+            c: 'BOITATÁ',
+            d: 'SACI-PERERÊ',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    5= {
+        pergunta: 'Fidel Castro nasceu em que país?',
+        alternativas: {
+            a: 'JAMAICA',
+            b: 'CUBA',
+            c: 'EL SALVADOR',
+            d: 'MÉXICO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    6= {
+        pergunta: 'Quem proclamou a república no Brasil em 1889?',
+        alternativas: {
+            a: 'DUQUE DE CAXIAS',
+            b: 'MARECHAL RONDON',
+            c: 'DOM PEDRO II',
+            d: 'MARECHAL DEODORO',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    7= {
+        pergunta: 'Quem é o patrono do exército brasileiro?',
+        alternativas: {
+            a: 'MARECHAL DEODORO',
+            b: 'BARÃO DE MAUÁ',
+            c: 'DUQUE DE CAXIAS',
+            d: 'MARQUÊS DE POMBAL',  
+        },
+        resposta: alternativas.c
+    
+    },
+
+    8= {
+        pergunta: 'Quem era o apresentador que reprovava os calouros tocandouma buzina?',
+        alternativas: {
+            a: 'RAUL GIL',
+            b: 'BOLINHA',
+            c: 'FLÁVIO CAVALCANTI',
+            d: 'CHACRINHA',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    9= {
+        pergunta: 'O que era Frankenstein, de Mary Shelley?',
+        alternativas: {
+            a: 'MONSTRO',
+            b: 'GORILA',
+            c: 'PRÍNCIPE',
+            d: 'SAPO',  
+        },
+        resposta: alternativas.a
+    
+    },
 ];
 
-const perguntasNivel2 = [
+const perguntasLista2 = [
     0= {
-        pergunta: '',
+        pergunta: 'Qual é o signo do zodíaco de quem nasce no dia do ano-novo?',
         alternativas: {
-            a: '',
-            b: '',
-            c: '',
-            d: '',  
+            a: 'VIRGEM',
+            b: 'AQUÁRIO',
+            c: 'CAPRICÓRNIO',
+            d: 'ÁRIES',  
         },
-        resposta: ''
+        resposta: alternativas.c
     
     },
 
-];
-
-const perguntasNivel2 = [
-    0= {
-        pergunta: '',
+    1= {
+        pergunta: 'Quem fundou a fábrica de automóveis Ford?',
         alternativas: {
-            a: '',
-            b: '',
-            c: '',
-            d: '',  
+            a: 'HARRISON FORD',
+            b: 'GERALD FORD',
+            c: 'HENRY FORD',
+            d: 'ANNA FORD',  
         },
-        resposta: ''
+        resposta: alternativas.c
     
     },
 
+    2= {
+        pergunta: 'Qual é a cor que se associa com os grupos ecológicos?',
+        alternativas: {
+            a: 'PRETA',
+            b: 'VERMELHA',
+            c: 'AZUL',
+            d: 'VERDE',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    3= {
+        pergunta: 'A água ferve a quantos graus centígrados?',
+        alternativas: {
+            a: '200',
+            b: '100',
+            c: '170',
+            d: '220',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    4= {
+        pergunta: 'Qual lugar é também chamado de Santa Sé?',
+        alternativas: {
+            a: 'VENEZA',
+            b: 'VITÓRIA',
+            c: 'VANCOUVER',
+            d: 'VATICANO',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    5= {
+        pergunta: 'Quando é comemorado o dia da independência do Brasil?',
+        alternativas: {
+            a: '21 DE ABRIL',
+            b: '12 DE OUTUBRO',
+            c: '7 DE SETEMBRO',
+            d: '25 DE DEZEMBRO',  
+        },
+        resposta: alternativas.c
+    
+    },
+
+    6= {
+        pergunta: 'Quem tem por lema a frase: “Sempre alerta”?',
+        alternativas: {
+            a: 'MÉDICOS',
+            b: 'ESCOTEIROS',
+            c: 'BOMBEIROS',
+            d: 'POLICIAIS',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    7= {
+        pergunta: 'Quem foi o grande amor de Julieta?',
+        alternativas: {
+            a: 'ROMEU',
+            b: 'ORFEU',
+            c: 'HAMLET',
+            d: 'IAGO',  
+        },
+        resposta: alternativas.a
+    
+    },
+
+    8= {
+        pergunta: 'Quantos signos formam o zodíaco?',
+        alternativas: {
+            a: 'NOVE',
+            b: 'DEZ',
+            c: 'ONZE',
+            d: 'DOZE',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    9= {
+        pergunta: 'Vatapá é uma comida típica de qual estado?',
+        alternativas: {
+            a: 'PARANÁ',
+            b: 'SANTA CATARINA',
+            c: 'SÃO PAULO',
+            d: 'BAHIA',  
+        },
+        resposta: alternativas.d
+    
+    },
+    
+]
+
+const perguntasLista2 = [
+    0= {
+        pergunta: 'Como se chama o lugar onde são armazenadas as balas deum revólver',
+        alternativas: {
+            a: 'CANO',
+            b: 'TAMBOR',
+            c: 'AGULHA',
+            d: 'GATILHO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    1= {
+        pergunta: 'Qual personagem da turma da Mônica tem apenas cinco fiosde cabelo?',
+        alternativas: {
+            a: 'MÔNICA',
+            b: 'CEBOLINHA',
+            c: 'CASCÃO',
+            d: 'MAGALI',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    2= {
+        pergunta: 'Qual cantora tinha o apelido de “Ternurinha” na época da jovemguarda?',
+        alternativas: {
+            a: 'SILVINHA',
+            b: 'WANDERLÉIA',
+            c: 'GRETCHEN',
+            d: 'MARTINHA',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    3= {
+        pergunta: 'O churrasco é considerado uma comida típica de qual estado?',
+        alternativas: {
+            a: 'CEARÁ',
+            b: 'RIO GRANDE DO SUL',
+            c: 'PARÁ',
+            d: 'MARANHÃO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    4= {
+        pergunta: 'Quem foi o criador dospersonagens Pedrinho,Narizinho e Emília?',
+        alternativas: {
+            a: 'MAURÍCIO DE SOUSA',
+            b: 'ZIRALDO',
+            c: 'MONTEIRO LOBATO',
+            d: 'MACHADO DE ASSIS',  
+        },
+        resposta: alternativas.c
+    
+    },
+
+    5= {
+        pergunta: 'Qual cantor é o pai da dupla Sandy e Júnior?',
+        alternativas: {
+            a: 'LEONARDO',
+            b: 'XORORÓ',
+            c: 'ZEZÉ DI CAMARGO',
+            d: 'CHITÃOZINHO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    6= {
+        pergunta: 'Quem fundou a Microsoft?',
+        alternativas: {
+            a: 'SULTÃO DE BRUNEI',
+            b: 'AKIO MORITA',
+            c: 'BILL GATES',
+            d: 'PRÍNCIPE CHARLES',  
+        },
+        resposta: alternativas.c
+    
+    },
+
+    7= {
+        pergunta: 'Qual é o nome dado ao pneu reserva do carro?',
+        alternativas: {
+            a: 'CALOTA',
+            b: 'ESTEPE',
+            c: 'MACACO',
+            d: 'CHAVE DE RODA',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    8= {
+        pergunta: 'Qual é o nome do cachorro medroso do Salsicha, dos desenhos animados?',
+        alternativas: {
+            a: 'FLOQUINHO',
+            b: 'RIN-TIN-TIN',
+            c: 'LASSIE',
+            d: 'SCOOBY-DOO',  
+        },
+        resposta: alternativas.d
+    
+    },
+
+    9= {
+        pergunta: 'Quantas dentições naturais temo ser humano durante a vida',
+        alternativas: {
+            a: 'UMA',
+            b: 'DUAS',
+            c: 'TRÊS',
+            d: 'QUATRO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+    
 ]
 
