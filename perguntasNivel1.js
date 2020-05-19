914c42fecc8c1f5aa71ca493507196f186e9530d
vinnidias <vinicius.3991@gmail.com>
2020-05-19 11:52:35 -0300

    estrutura das perguntas correto

---
+++ perguntasNivel1.js
@@ -0,0 +1,45 @@
+const perguntasNivel1 = [
+    0= {
+        pergunta: '',
+        alternativas: {
+            a: '',
+            b: '',
+            c: '',
+            d: '',  
+        },
+        resposta: ''
+    
+    },
+
+];
+
+const perguntasNivel2 = [
+    0= {
+        pergunta: '',
+        alternativas: {
+            a: '',
+            b: '',
+            c: '',
+            d: '',  
+        },
+        resposta: ''
+    
+    },
+
+];
+
+const perguntasNivel2 = [
+    0= {
+        pergunta: '',
+        alternativas: {
+            a: '',
+            b: '',
+            c: '',
+            d: '',  
+        },
+        resposta: ''
+    
+    },
+
+]
+
