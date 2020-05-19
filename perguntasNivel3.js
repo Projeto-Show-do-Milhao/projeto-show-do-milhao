2a66fdb83a00816a91cbfadd1ebd6cdfe3135b09
sguilherme-1995 <guilhermenunes26@gmail.com>
2020-05-19 13:03:43 -0300

    nivel3

---
+++ perguntasNivel3.js
@@ -0,0 +1,343 @@
+const perguntasLista31 = [
+    0= {
+        pergunta: 'Em qual espécie o macho choca os ovos e a fêmea procura alimento?',
+        alternativas: {
+            a: 'ANDORINHA',
+            b: 'PATO SELVAGEM',
+            c: 'PINGÜIM',
+            d: 'MARRECO',  
+        },
+        resposta: alternativas.b
+    
+    },
+    1= {
+        pergunta: 'Em qual país está localizado o “Muro das lamentações”?',
+        alternativas: {
+            a: 'ALEMANHA',
+            b: 'BRASIL',
+            c: 'VENEZUELA',
+            d: 'ISRAEL',  
+        },
+        resposta: alternativas.d
+    
+    },
+    2= {
+        pergunta: 'Qual desses países não fica na Ásia?',
+        alternativas: {
+            a: 'PAQUISTÃO',
+            b: 'JAPÃO',
+            c: 'TAILÂNDIA',
+            d: 'EGITO',  
+        },
+        resposta: alternativas.d
+    
+    },
+    3= {
+        pergunta: 'Qual desses astros de filmes de ação é belga?',
+        alternativas: {
+            a: 'ARNOLD SCHWARZENEGGER',
+            b: 'SYLVESTER STALLONE',
+            c: 'STEVEN SEAGAL',
+            d: 'JEAN CLAUDE VAN DAMME',  
+        },
+        resposta: alternativas.d
+    
+    },
+    4= {
+        pergunta: 'Onde foi conduzida a vitória das forças aliadas na Segunda Guerra Mundial?',
+        alternativas: {
+            a: 'CANNES',
+            b: 'NORMANDIA',
+            c: 'CAPRI',
+            d: 'MARSELHA',  
+        },
+        resposta: alternativas.b
+    
+    },
+    5= {
+        pergunta: 'Onde nasceu Van Gogh, o grande pintor impressionista?',
+        alternativas: {
+            a: 'POLÔNIA',
+            b: 'FRANÇA',
+            c: 'ITÁLIA',
+            d: 'HOLANDA',  
+        },
+        resposta: alternativas.d
+    
+    },
+    6= {
+        pergunta: 'Qual é a primeira letra do alfabeto grego?',
+        alternativas: {
+            a: 'DELTA',
+            b: 'BETA',
+            c: 'ALFA',
+            d: 'GAMA',  
+        },
+        resposta: alternativas.c
+    
+    },
+    7= {
+        pergunta: 'O que significa literalmente Perestroika?',
+        alternativas: {
+            a: 'CONVERSÃO',
+            b: 'INVOLUÇÃO',
+            c: 'REESTRUTURAÇÃO',
+            d: 'REGRESSÃO',  
+        },
+        resposta: alternativas.c
+    
+    },
+    8= {
+        pergunta: 'Qual desses quatro pesos é o mais leve?',
+        alternativas: {
+            a: '10 ONÇAS',
+            b: '10 GRAMAS',
+            c: '10 QUILOS',
+            d: '10 LIBRAS',  
+        },
+        resposta: alternativas.b
+    
+    },
+    9= {
+        pergunta: 'Brahma é o deus de que religião?',
+        alternativas: {
+            a: 'HINDUÍSMO',
+            b: 'XINTOÍSMO',
+            c: 'BUDISMO',
+            d: 'ISLAMISMO',  
+        },
+        resposta: alternativas.a
+    
+    },
+
+];
+
+const perguntasLista32 = [
+    0= {
+        pergunta: 'Os nazistas foram julgados em:',
+        alternativas: {
+            a: 'BERLIM',
+            b: 'NUREMBERGUE',
+            c: 'MUNIQUE',
+            d: 'PARIS',  
+        },
+        resposta: alternativas.b
+    
+    },
+    1= {
+        pergunta: 'Qual oceano tem o maior volume de água?',
+        alternativas: {
+            a: 'ATLÂNTICO',
+            b: 'PACÍFICO',
+            c: 'ÍNDICO',
+            d: 'PARIS',  
+        },
+        resposta: alternativas.b
+    
+    },
+    2= {
+        pergunta: 'Como o rei francês Luis XVI e sua esposa Maria Antonieta morreram?',
+        alternativas: {
+            a: 'ENFORCADOS',
+            b: 'FUZILADOS',
+            c: 'GUILHOTINADOS',
+            d: 'QUEIMADOS',  
+        },
+        resposta: alternativas.c
+    
+    },
+    3= {
+        pergunta: 'Que conflito ideológico envolveu os EUA e a União Soviética?',
+        alternativas: {
+            a: 'GUERRA FRIA',
+            b: 'GUERRA DO VIETNÃ',
+            c: 'GUERRA NAS ESTRELAS',
+            d: 'GUERRA DA CORÉIA',  
+        },
+        resposta: alternativas.a
+    
+    },
+    4= {
+        pergunta: 'O trapézio é um músculo que está situado:',
+        alternativas: {
+            a: 'NO PESCOÇO',
+            b: 'NO OMBRO',
+            c: 'NA CABEÇA',
+            d: 'NO BRAÇO',  
+        },
+        resposta: alternativas.b
+    
+    },
+    5= {
+        pergunta: 'Quem escreveu o livro “A sangue frio” em 1966?',
+        alternativas: {
+            a: 'TRUMAN CAPOTE',
+            b: 'HENRY JAMES',
+            c: 'JOHN STEINBECK',
+            d: 'TONI MORRISON',  
+        },
+        resposta: alternativas.a
+    
+    },
+    6= {
+        pergunta: 'Quem escreveu “Ulisses” em 1922?',
+        alternativas: {
+            a: 'ERNEST HEMINGWAY',
+            b: 'MARCEL PROUST',
+            c: 'T.S. ELLIOT',
+            d: 'JAMES JOYCE',  
+        },
+        resposta: alternativas.d
+    
+    },
+    7= {
+        pergunta: 'Qual o símbolo químico do radônio?',
+        alternativas: {
+            a: 'Rr',
+            b: 'Rr',
+            c: 'Rn',
+            d: 'Ro',  
+        },
+        resposta: alternativas.c
+    
+    },
+    8= {
+        pergunta: 'O que são meninges?',
+        alternativas: {
+            a: 'CÉLULAS',
+            b: 'NERVOS',
+            c: 'MEMBRANAS',
+            d: 'MÚSCULOS',  
+        },
+        resposta: alternativas.c
+    
+    },
+    9= {
+        pergunta: 'Qual produto gerou guerras e conflitos no século XX?',
+        alternativas: {
+            a: 'ÁLCOOL',
+            b: 'PETRÓLEO',
+            c: 'OURO',
+            d: 'ALUMÍNIO',  
+        },
+        resposta: alternativas.b
+    
+    },
+
+];
+
+const perguntasLista33 = [
+    0= {
+        pergunta: 'Que ramo da pecuária ocupase de bodes e cabras?',
+        alternativas: {
+            a: 'ASININO',
+            b: 'CAPRINO',
+            c: 'BUFALINO',
+            d: 'MUAR',  
+        },
+        resposta: alternativas.b
+    
+    },
+    1= {
+        pergunta: 'Como é chamada a bola de gelo e poeira que orbita em torno do sol?',
+        alternativas: {
+            a: 'COMETA',
+            b: 'METEORITO',
+            c: 'CAMADA DE OZÔNIO',
+            d: 'ESTRELA D’ALVA',  
+        },
+        resposta: alternativas.a
+    
+    },
+    2= {
+        pergunta: 'O confucionismo é uma filosofia de qual nacionalidade?',
+        alternativas: {
+            a: 'INDIANA',
+            b: 'JAPONESA',
+            c: 'CHINESA',
+            d: 'COREANA',  
+        },
+        resposta: alternativas.c
+    
+    },
+    3= {
+        pergunta: 'Qual a ciência que estuda a distribuição dos animais?',
+        alternativas: {
+            a: 'ZOOTERAPIA',
+            b: 'ZOOGEOGRAFIA',
+            c: 'ZODÍACO',
+            d: 'ZOOMETRIA',  
+        },
+        resposta: alternativas.b
+    
+    },
+    4= {
+        pergunta: 'A eletrônica é parte de qual ciência?',
+        alternativas: {
+            a: 'FÍSICA',
+            b: 'BIOLOGIA',
+            c: 'QUÍMICA',
+            d: 'BOTÂNICA',  
+        },
+        resposta: alternativas.a
+    
+    },
+    5= {
+        pergunta: 'A Bélgica é:',
+        alternativas: {
+            a: 'UMA REPÚBLICA',
+            b: '2 UMA MONARQUIA CONSTITUCIONAL',
+            c: 'UM EMIRADO',
+            d: 'UM PRINCIPADO',  
+        },
+        resposta: alternativas.b
+    
+    },
+    6= {
+        pergunta: 'Que parte do corpo humano é infectada pela cólera?',
+        alternativas: {
+            a: 'GARGANTA',
+            b: 'INTESTINO',
+            c: 'PULMÕES',
+            d: 'RINS',  
+        },
+        resposta: alternativas.b
+    
+    },
+    7= {
+        pergunta: 'Que figura mitológica é conhecida por sua força extraordinária?',
+        alternativas: {
+            a: 'ÁTILA',
+            b: 'MINOTAURO',
+            c: 'PERSEU',
+            d: 'HÉRCULES',  
+        },
+        resposta: alternativas.d
+    
+    },
+    8= {
+        pergunta: 'A união do espermatozóide com o óvulo origina uma célula chamada:',
+        alternativas: {
+            a: 'ZIGOTO',
+            b: 'BIGOTO',
+            c: 'FETO',
+            d: 'GAROTO',  
+        },
+        resposta: alternativas.a
+    
+    },
+    9= {
+        pergunta: 'A que país pertence a ilha de Terra Nova?',
+        alternativas: {
+            a: 'ESTADOS UNIDOS',
+            b: 'DINAMARCA',
+            c: 'CANADÁ',
+            d: 'FRANÇA',  
+        },
+        resposta: alternativas.c
+    
+    },
+
+
+]
+
