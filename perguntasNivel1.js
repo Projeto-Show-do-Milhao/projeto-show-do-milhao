81f99fcea5f3171ee6496060ec24d29538bdbd32
sguilherme-1995 <guilhermenunes26@gmail.com>
2020-05-28 14:25:45 -0300

    resposta para string

--- perguntasNivel1.js
+++ perguntasNivel1.js
@@ -7,8 +7,9 @@ const perguntasListal = [
 					b: 'RIO GRANDE DO SUL',
 					c: 'SANTA CATARINA',
 					d: 'GOIÁS',  
-			},
-			resposta: alternativas.b
+            },
+            resposta: 'b'
+			// resposta: alternativas.b
 	
 	},
 
@@ -19,8 +20,9 @@ const perguntasListal = [
 					b: 'SÓLIDO',
 					c: 'GASOSO',
 					d: 'VAPOROSO',  
-			},
-			resposta: alternativas.b
+            },
+            resposta: 'b'
+			// resposta: alternativas.b
 	
 	},
 	
@@ -31,8 +33,9 @@ const perguntasListal = [
 					b: 'PAULISTINHA',
 					c: 'PIMENTINHA',
 					d: 'ANDORINHA',  
-			},
-			resposta: alternativas.c
+            },
+            resposta: 'c'
+			// resposta: alternativas.c
 	
 	},
 
@@ -43,8 +46,9 @@ const perguntasListal = [
 					b: 'MINNIE',
 					c: 'A PEQUENA SEREIA',
 					d: 'OLÍVIA PALITO',  
-			},
-			resposta: alternativas.b
+            },
+            resposta: 'b'
+			// resposta: alternativas.b
 	
 	},
 
@@ -55,8 +59,9 @@ const perguntasListal = [
 					b: 'NEGRINHO DO PASTOREIO',
 					c: 'BOITATÁ',
 					d: 'SACI-PERERÊ',  
-			},
-			resposta: alternativas.d
+            },
+            resposta: 'd'
+			// resposta: alternativas.d
 	
 	},
 
@@ -67,8 +72,9 @@ const perguntasListal = [
 					b: 'CUBA',
 					c: 'EL SALVADOR',
 					d: 'MÉXICO',  
-			},
-			resposta: alternativas.b
+            },
+            resposta: 'b'
+			// resposta: alternativas.b
 	
 	},
 
@@ -79,8 +85,9 @@ const perguntasListal = [
 					b: 'MARECHAL RONDON',
 					c: 'DOM PEDRO II',
 					d: 'MARECHAL DEODORO',  
-			},
-			resposta: alternativas.d
+            },
+            resposta: 'd'
+			// resposta: alternativas.d
 	
 	},
 
@@ -91,8 +98,9 @@ const perguntasListal = [
 					b: 'BARÃO DE MAUÁ',
 					c: 'DUQUE DE CAXIAS',
 					d: 'MARQUÊS DE POMBAL',  
-			},
-			resposta: alternativas.c
+            },
+            resposta: 'c'
+			// resposta: alternativas.c
 	
 	},
 
@@ -103,8 +111,9 @@ const perguntasListal = [
 					b: 'BOLINHA',
 					c: 'FLÁVIO CAVALCANTI',
 					d: 'CHACRINHA',  
-			},
-			resposta: alternativas.d
+            },
+            resposta: 'd'
+			// resposta: alternativas.d
 	
 	},
 
@@ -115,256 +124,257 @@ const perguntasListal = [
 					b: 'GORILA',
 					c: 'PRÍNCIPE',
 					d: 'SAPO',  
-			},
-			resposta: alternativas.a
+            },
+            resposta: 'a'
+			// resposta: alternativas.a
 	
 	},
 ];
 
-const perguntasLista2 = [
-    0= {
-        pergunta: 'Qual é o signo do zodíaco de quem nasce no dia do ano-novo?',
-        alternativas: {
-            a: 'VIRGEM',
-            b: 'AQUÁRIO',
-            c: 'CAPRICÓRNIO',
-            d: 'ÁRIES',  
-        },
-        resposta: alternativas.c
+// const perguntasLista2 = [
+//     0= {
+//         pergunta: 'Qual é o signo do zodíaco de quem nasce no dia do ano-novo?',
+//         alternativas: {
+//             a: 'VIRGEM',
+//             b: 'AQUÁRIO',
+//             c: 'CAPRICÓRNIO',
+//             d: 'ÁRIES',  
+//         },
+//         resposta: alternativas.c
 	
-    },
+//     },
 
-    1= {
-        pergunta: 'Quem fundou a fábrica de automóveis Ford?',
-        alternativas: {
-            a: 'HARRISON FORD',
-            b: 'GERALD FORD',
-            c: 'HENRY FORD',
-            d: 'ANNA FORD',  
-        },
-        resposta: alternativas.c
+//     1= {
+//         pergunta: 'Quem fundou a fábrica de automóveis Ford?',
+//         alternativas: {
+//             a: 'HARRISON FORD',
+//             b: 'GERALD FORD',
+//             c: 'HENRY FORD',
+//             d: 'ANNA FORD',  
+//         },
+//         resposta: alternativas.c
 	
-    },
+//     },
 
-    2= {
-        pergunta: 'Qual é a cor que se associa com os grupos ecológicos?',
-        alternativas: {
-            a: 'PRETA',
-            b: 'VERMELHA',
-            c: 'AZUL',
-            d: 'VERDE',  
-        },
-        resposta: alternativas.d
+//     2= {
+//         pergunta: 'Qual é a cor que se associa com os grupos ecológicos?',
+//         alternativas: {
+//             a: 'PRETA',
+//             b: 'VERMELHA',
+//             c: 'AZUL',
+//             d: 'VERDE',  
+//         },
+//         resposta: alternativas.d
 	
-    },
+//     },
 
-    3= {
-        pergunta: 'A água ferve a quantos graus centígrados?',
-        alternativas: {
-            a: '200',
-            b: '100',
-            c: '170',
-            d: '220',  
-        },
-        resposta: alternativas.b
+//     3= {
+//         pergunta: 'A água ferve a quantos graus centígrados?',
+//         alternativas: {
+//             a: '200',
+//             b: '100',
+//             c: '170',
+//             d: '220',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    4= {
-        pergunta: 'Qual lugar é também chamado de Santa Sé?',
-        alternativas: {
-            a: 'VENEZA',
-            b: 'VITÓRIA',
-            c: 'VANCOUVER',
-            d: 'VATICANO',  
-        },
-        resposta: alternativas.d
+//     4= {
+//         pergunta: 'Qual lugar é também chamado de Santa Sé?',
+//         alternativas: {
+//             a: 'VENEZA',
+//             b: 'VITÓRIA',
+//             c: 'VANCOUVER',
+//             d: 'VATICANO',  
+//         },
+//         resposta: alternativas.d
 	
-    },
+//     },
 
-    5= {
-        pergunta: 'Quando é comemorado o dia da independência do Brasil?',
-        alternativas: {
-            a: '21 DE ABRIL',
-            b: '12 DE OUTUBRO',
-            c: '7 DE SETEMBRO',
-            d: '25 DE DEZEMBRO',  
-        },
-        resposta: alternativas.c
+//     5= {
+//         pergunta: 'Quando é comemorado o dia da independência do Brasil?',
+//         alternativas: {
+//             a: '21 DE ABRIL',
+//             b: '12 DE OUTUBRO',
+//             c: '7 DE SETEMBRO',
+//             d: '25 DE DEZEMBRO',  
+//         },
+//         resposta: alternativas.c
 	
-    },
+//     },
 
-    6= {
-        pergunta: 'Quem tem por lema a frase: “Sempre alerta”?',
-        alternativas: {
-            a: 'MÉDICOS',
-            b: 'ESCOTEIROS',
-            c: 'BOMBEIROS',
-            d: 'POLICIAIS',  
-        },
-        resposta: alternativas.b
+//     6= {
+//         pergunta: 'Quem tem por lema a frase: “Sempre alerta”?',
+//         alternativas: {
+//             a: 'MÉDICOS',
+//             b: 'ESCOTEIROS',
+//             c: 'BOMBEIROS',
+//             d: 'POLICIAIS',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    7= {
-        pergunta: 'Quem foi o grande amor de Julieta?',
-        alternativas: {
-            a: 'ROMEU',
-            b: 'ORFEU',
-            c: 'HAMLET',
-            d: 'IAGO',  
-        },
-        resposta: alternativas.a
+//     7= {
+//         pergunta: 'Quem foi o grande amor de Julieta?',
+//         alternativas: {
+//             a: 'ROMEU',
+//             b: 'ORFEU',
+//             c: 'HAMLET',
+//             d: 'IAGO',  
+//         },
+//         resposta: alternativas.a
 	
-    },
+//     },
 
-    8= {
-        pergunta: 'Quantos signos formam o zodíaco?',
-        alternativas: {
-            a: 'NOVE',
-            b: 'DEZ',
-            c: 'ONZE',
-            d: 'DOZE',  
-        },
-        resposta: alternativas.d
+//     8= {
+//         pergunta: 'Quantos signos formam o zodíaco?',
+//         alternativas: {
+//             a: 'NOVE',
+//             b: 'DEZ',
+//             c: 'ONZE',
+//             d: 'DOZE',  
+//         },
+//         resposta: alternativas.d
 	
-    },
+//     },
 
-    9= {
-        pergunta: 'Vatapá é uma comida típica de qual estado?',
-        alternativas: {
-            a: 'PARANÁ',
-            b: 'SANTA CATARINA',
-            c: 'SÃO PAULO',
-            d: 'BAHIA',  
-        },
-        resposta: alternativas.d
+//     9= {
+//         pergunta: 'Vatapá é uma comida típica de qual estado?',
+//         alternativas: {
+//             a: 'PARANÁ',
+//             b: 'SANTA CATARINA',
+//             c: 'SÃO PAULO',
+//             d: 'BAHIA',  
+//         },
+//         resposta: alternativas.d
 	
-    },
+//     },
 	
-]
+// ]
 
-const perguntasLista2 = [
-    0= {
-        pergunta: 'Como se chama o lugar onde são armazenadas as balas deum revólver',
-        alternativas: {
-            a: 'CANO',
-            b: 'TAMBOR',
-            c: 'AGULHA',
-            d: 'GATILHO',  
-        },
-        resposta: alternativas.b
+// const perguntasLista2 = [
+//     0= {
+//         pergunta: 'Como se chama o lugar onde são armazenadas as balas deum revólver',
+//         alternativas: {
+//             a: 'CANO',
+//             b: 'TAMBOR',
+//             c: 'AGULHA',
+//             d: 'GATILHO',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    1= {
-        pergunta: 'Qual personagem da turma da Mônica tem apenas cinco fiosde cabelo?',
-        alternativas: {
-            a: 'MÔNICA',
-            b: 'CEBOLINHA',
-            c: 'CASCÃO',
-            d: 'MAGALI',  
-        },
-        resposta: alternativas.b
+//     1= {
+//         pergunta: 'Qual personagem da turma da Mônica tem apenas cinco fiosde cabelo?',
+//         alternativas: {
+//             a: 'MÔNICA',
+//             b: 'CEBOLINHA',
+//             c: 'CASCÃO',
+//             d: 'MAGALI',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    2= {
-        pergunta: 'Qual cantora tinha o apelido de “Ternurinha” na época da jovemguarda?',
-        alternativas: {
-            a: 'SILVINHA',
-            b: 'WANDERLÉIA',
-            c: 'GRETCHEN',
-            d: 'MARTINHA',  
-        },
-        resposta: alternativas.b
+//     2= {
+//         pergunta: 'Qual cantora tinha o apelido de “Ternurinha” na época da jovemguarda?',
+//         alternativas: {
+//             a: 'SILVINHA',
+//             b: 'WANDERLÉIA',
+//             c: 'GRETCHEN',
+//             d: 'MARTINHA',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    3= {
-        pergunta: 'O churrasco é considerado uma comida típica de qual estado?',
-        alternativas: {
-            a: 'CEARÁ',
-            b: 'RIO GRANDE DO SUL',
-            c: 'PARÁ',
-            d: 'MARANHÃO',  
-        },
-        resposta: alternativas.b
+//     3= {
+//         pergunta: 'O churrasco é considerado uma comida típica de qual estado?',
+//         alternativas: {
+//             a: 'CEARÁ',
+//             b: 'RIO GRANDE DO SUL',
+//             c: 'PARÁ',
+//             d: 'MARANHÃO',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    4= {
-        pergunta: 'Quem foi o criador dospersonagens Pedrinho,Narizinho e Emília?',
-        alternativas: {
-            a: 'MAURÍCIO DE SOUSA',
-            b: 'ZIRALDO',
-            c: 'MONTEIRO LOBATO',
-            d: 'MACHADO DE ASSIS',  
-        },
-        resposta: alternativas.c
+//     4= {
+//         pergunta: 'Quem foi o criador dospersonagens Pedrinho,Narizinho e Emília?',
+//         alternativas: {
+//             a: 'MAURÍCIO DE SOUSA',
+//             b: 'ZIRALDO',
+//             c: 'MONTEIRO LOBATO',
+//             d: 'MACHADO DE ASSIS',  
+//         },
+//         resposta: alternativas.c
 	
-    },
+//     },
 
-    5= {
-        pergunta: 'Qual cantor é o pai da dupla Sandy e Júnior?',
-        alternativas: {
-            a: 'LEONARDO',
-            b: 'XORORÓ',
-            c: 'ZEZÉ DI CAMARGO',
-            d: 'CHITÃOZINHO',  
-        },
-        resposta: alternativas.b
+//     5= {
+//         pergunta: 'Qual cantor é o pai da dupla Sandy e Júnior?',
+//         alternativas: {
+//             a: 'LEONARDO',
+//             b: 'XORORÓ',
+//             c: 'ZEZÉ DI CAMARGO',
+//             d: 'CHITÃOZINHO',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    6= {
-        pergunta: 'Quem fundou a Microsoft?',
-        alternativas: {
-            a: 'SULTÃO DE BRUNEI',
-            b: 'AKIO MORITA',
-            c: 'BILL GATES',
-            d: 'PRÍNCIPE CHARLES',  
-        },
-        resposta: alternativas.c
+//     6= {
+//         pergunta: 'Quem fundou a Microsoft?',
+//         alternativas: {
+//             a: 'SULTÃO DE BRUNEI',
+//             b: 'AKIO MORITA',
+//             c: 'BILL GATES',
+//             d: 'PRÍNCIPE CHARLES',  
+//         },
+//         resposta: alternativas.c
 	
-    },
+//     },
 
-    7= {
-        pergunta: 'Qual é o nome dado ao pneu reserva do carro?',
-        alternativas: {
-            a: 'CALOTA',
-            b: 'ESTEPE',
-            c: 'MACACO',
-            d: 'CHAVE DE RODA',  
-        },
-        resposta: alternativas.b
+//     7= {
+//         pergunta: 'Qual é o nome dado ao pneu reserva do carro?',
+//         alternativas: {
+//             a: 'CALOTA',
+//             b: 'ESTEPE',
+//             c: 'MACACO',
+//             d: 'CHAVE DE RODA',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
-    8= {
-        pergunta: 'Qual é o nome do cachorro medroso do Salsicha, dos desenhos animados?',
-        alternativas: {
-            a: 'FLOQUINHO',
-            b: 'RIN-TIN-TIN',
-            c: 'LASSIE',
-            d: 'SCOOBY-DOO',  
-        },
-        resposta: alternativas.d
+//     8= {
+//         pergunta: 'Qual é o nome do cachorro medroso do Salsicha, dos desenhos animados?',
+//         alternativas: {
+//             a: 'FLOQUINHO',
+//             b: 'RIN-TIN-TIN',
+//             c: 'LASSIE',
+//             d: 'SCOOBY-DOO',  
+//         },
+//         resposta: alternativas.d
 	
-    },
+//     },
 
-    9= {
-        pergunta: 'Quantas dentições naturais temo ser humano durante a vida',
-        alternativas: {
-            a: 'UMA',
-            b: 'DUAS',
-            c: 'TRÊS',
-            d: 'QUATRO',  
-        },
-        resposta: alternativas.b
+//     9= {
+//         pergunta: 'Quantas dentições naturais temo ser humano durante a vida',
+//         alternativas: {
+//             a: 'UMA',
+//             b: 'DUAS',
+//             c: 'TRÊS',
+//             d: 'QUATRO',  
+//         },
+//         resposta: alternativas.b
 	
-    },
+//     },
 
 	
-]
+// ]
 
