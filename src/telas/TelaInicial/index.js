5f1e4f785f1f55f68632e908d39c6bd7ce1ec034
mfidelis1994 <macielf1994@gmail.com>
2020-05-30 14:27:23 -0300

    Implementando BotaoFacebook

--- src/telas/TelaInicial/index.js
+++ src/telas/TelaInicial/index.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import { View, ImageBackground } from 'react-native'
 import estilos from '../../telas/TelaInicial/styles';
+import * as Facebook from 'expo-facebook';
+
+const id = '248425269594978'
+
+login = async () => {
+
+  await Facebook.initializeAsync('248425269594978');
+
+  const {type, token} = await Facebook.logInWithReadPermissionsAsync(id, {permissions: ['public_profile', 'email', 'user_friends']})
+
+  if (type === 'success') {
+
+    const response = await fetch(
+      'https://graph.facebook.com/me?access.token=$[token]&fields=id,name,email,about,picture'
+
+      )
+      console.log('resposta:', response)
+
+      const json = await response.json()
+
+      console.log('USER_INFO', json)
+
+      firebase.auth().signInWithCredential(token)
+
+  } else {
+    alert(type);
+  }
+}
 
 function TelaInicial() {
   return (
