5e7479fe0a1f25a0cc7f7549fbce3cf2ffdfba31
sguilherme-1995 <guilhermenunes26@gmail.com>
2020-06-01 18:04:40 -0300

    voltando do perfil para o lobby

--- src/telas/TelaPerfil/index.js
+++ src/telas/TelaPerfil/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { View, Text, ImageBackground, Image } from 'react-native';
 import estilos from './styles'
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import { TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
 
-function TelaPerfil() {
+function TelaPerfil({navigation}) {
   return (
       <View style={estilos.containerTelaPerfil}>
           <Text>Oi</Text>
@@ -14,9 +14,11 @@ function TelaPerfil() {
               />
           </View>
           <View style={estilos.containerLogoEAvatar}>
+              <TouchableOpacity onPress={()=> navigation.navigate("TelaLobby")}>
               <Image source={require('../../imagens/showDoMilhaoBackground.png')} 
                 style={estilos.imagemDeFundoShowDoMilhaoTelaPerfil} 
               />
+              </TouchableOpacity>
               <Image source={require('../../imagens/chico1.png')} style={estilos.avatarTelaPerfil} />
           </View>
           <View style={estilos.containerNickName}>
