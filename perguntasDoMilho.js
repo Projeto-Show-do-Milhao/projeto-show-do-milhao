ac5d885b06a75ab42e9f8ff0edb8cbec7c322537
sguilherme-1995 <guilhermenunes26@gmail.com>
2020-06-01 20:32:29 -0300

    Perguntas do milhão

---
+++ perguntasDoMilho.js
@@ -0,0 +1,168 @@
+const perguntasListaMilho = [
+    {
+        pergunta: 'Qual o nome do prédio mais alto do mundo?',
+        alternativas: {
+            a: 'EMPIRE STATE BUILDING',
+            b: 'SHANGHAI TOWER',
+            c: 'BURJ KHALIFA',
+            d: 'MAKKAH CLOCK TOWER',  
+              },
+              resposta: 'BURJ KHALIFA'
+    
+    },
+    {
+        pergunta: 'Qual o nome do cachorro que de skate percorreu o maior túneu feito pelo homem? ',
+        alternativas: {
+            a: 'OTTO',
+            b: 'TOBBY',
+            c: 'BOBBY',
+            d: 'JORDAN',  
+              },
+              resposta: 'OTTO'
+    
+    },
+    {
+        pergunta: 'Qual o nome do homem mais alto vivo atualmente?',
+        alternativas: {
+            a: 'SULTAN KÖSEN',
+            b: 'ANKARA KÖSEN',
+            c: 'CHANDRA DANGI',
+            d: 'ROGER WATER',  
+              },
+              resposta: 'SULTAN KÖSEN'
+    
+    },
+    {
+        pergunta: 'Qual o nome da primeira pessoa a quebrar a barreira do som em queda livre?',
+        alternativas: {
+            a: 'FELIX MACLAINE',
+            b: 'GEORGE BRIDGE',
+            c: 'FELIX BAUMGARTNER',
+            d: 'JOSEPH KITTINGER',  
+              },
+              resposta: 'FELIX BAUMGARTNER'
+    
+    },
+    {
+        pergunta: 'Quem foi a primeira pessoa a quebrar uma milha em menos de quatro minutos?',
+        alternativas: {
+            a: 'EDMUND HILARY',
+            b: 'USAIN BOLT',
+            c: 'TENZING NORGAY',
+            d: 'ROGER BANNISTER',  
+              },
+              resposta: 'ROGER BANNISTER'
+    
+    },
+    {
+        pergunta: 'Qual o nome da pessoa mais jovem a ganhar o premio Nobel?',
+        alternativas: {
+            a: 'ASIF ALI ZARDARI',
+            b: 'MALALA YOUSAFZAI',
+            c: 'BENAZIR BHUTTO',
+            d: 'MIRZA WAHEED',  
+              },
+              resposta: 'MALALA YOUSAFZAI'
+    
+    },
+    {
+        pergunta: 'Qual o sobrenome das duas primeiras pessoas a pisar na lua?',
+        alternativas: {
+            a: 'ARMSTRONG E ALDRIN',
+            b: 'MACLAINE E ALDRIN',
+            c: 'ARMSTRONG E GAGARI',
+            d: 'MACLAINE E GAGARI',  
+              },
+              resposta: 'ARMSTRONG E ALDRIN'
+    
+    },
+    {
+        pergunta: 'Qual o nome da mulher a fazer a maior caminhada no espaço?',
+        alternativas: {
+            a: 'SALLY RIDE',
+            b: 'SUNITA WILLIAMS',
+            c: 'PEGGY WHITSON',
+            d: 'SALLY ARMSTRONG',  
+              },
+              resposta: 'PEGGY WHITSON'
+    
+    },
+    {
+        pergunta: 'Qual o nome da mulher mais baixa do mundo?',
+        alternativas: {
+            a: 'BIKKURI CHOJIN',
+            b: 'JYOTI KISANJI AMGE',
+            c: 'BRIDGETTE JORDAN',
+            d: 'MIKA SINGH',  
+              },
+              resposta: 'JYOTI KISANJI AMGE'
+    
+    },
+    {
+        pergunta: 'Qual o nome do conquistador com o maior império da história?',
+        alternativas: {
+            a: 'NAPOLEÃO',
+            b: 'GÊNGIS KHAN',
+            c: 'ALEXANDRE O GRANDE',
+            d: 'ADOLF HITLER',  
+              },
+              resposta: 'GÊNGIS KHAN'
+    
+    },
+    {
+        pergunta: 'Qual o nome da mulher responsável pela primeira foto de um buraco negro na história?',
+        alternativas: {
+            a: 'ADA LOVELACE',
+            b: 'KATHERINE JOHNSON',
+            c: 'KATIE BOUMAN',
+            d: 'MARIE CURIE',  
+              },
+              resposta: 'KATIE BOUMAN'
+    
+    },
+    {
+        pergunta: 'Qual o nome do maior cometa do sistema solar?',
+        alternativas: {
+            a: 'HALE-BOPP',
+            b: 'HARLEY',
+            c: 'OUMUAMUA',
+            d: 'SHOEMAKER-LEVY 9',  
+              },
+              resposta: 'HALE-BOPP'
+    
+    },
+    {
+        pergunta: 'Qual o nome da maior estrela do universo conhecida pelo homem?',
+        alternativas: {
+            a: 'SIRIUS',
+            b: 'VY CANIS MAJORIS',
+            c: 'UY SCUTI',
+            d: 'BETELGEUSE',  
+              },
+              resposta: 'VY CANIS MAJORIS'
+    
+    },
+    {
+        pergunta: 'Qual o nome da maior galaxia do universo observável?',
+        alternativas: {
+            a: 'VIA LACTEA',
+            b: 'IC 1101',
+            c: 'UGC 2885',
+            d: 'ANDROMEDA',  
+              },
+              resposta: 'UGC 2885'
+    
+    },
+    {
+        pergunta: 'Qual o nome do planeta mais quente do universo conhecido?',
+        alternativas: {
+            a: 'VÊNUS',
+            b: 'KELT-9B',
+            c: 'MARTE',
+            d: 'ALPHA 9',  
+              },
+              resposta: 'KELT-9B'
+    
+    },
+
+]
