1b9deedc8d3e0d1156c1bcc537842ff087c81c21
sguilherme-1995 <guilhermenunes26@gmail.com>
2020-06-11 16:29:22 -0300

    logout

--- src/telas/TelaPerfil/index.js
+++ src/telas/TelaPerfil/index.js
@@ -33,7 +33,7 @@ function TelaPerfil({route , navigation}) {
               <Text style={estilos.ultimaPontuacao}>Ultima Pontuacao</Text>
           </View>
           <View style={estilos.containerBotaoLogout}>
-              <TouchableHighlight style={estilos.botaoLogout}>
+              <TouchableHighlight style={estilos.botaoLogout} onPress={()=> navigation.navigate('TelaInicial')}>
               <Text style={estilos.textoBotaoLogout}>Logout</Text>
               </TouchableHighlight>
           </View>
